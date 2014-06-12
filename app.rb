944cb614900abe42289c7502af9e4e18f45ffe15
Carson McDonald <carson@ioncannon.net>
2014-06-12 10:59:59 -0400

    Add a little more info

--- app.rb
+++ app.rb
@@ -1,9 +1,18 @@
 require 'sinatra'
 
+set :sessions, true
+
 get '/' do
   File.read('index.html')
 end
 
 post '/demo' do
   "The username is #{params['username']} and the password is #{params['password']}"
+  session[:userid] = params['username']
+  session[:token] = 'token here'
+  redirect to '/videos'
+end
+
+get '/videos' do
+  "List of videos for #{session[:userid]} with token #{session[:token]}"
 end
